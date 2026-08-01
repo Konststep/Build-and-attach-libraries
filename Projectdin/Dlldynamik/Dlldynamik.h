a1239228b0cc982f06e1ff81a22739d35b0d246a
Konststep <netologykonst@gmail.com>
2026-08-01 22:53:10 +0300

    Add files via upload

---
+++ Projectdin/Dlldynamik/Dlldynamik.h
@@ -0,0 +1,18 @@
+#ifndef DLLDYNEMIK_H
+#define DLLDYNEMIK_H
+
+#ifdef DLLDYNEMIK_EXPORTS
+#define PROGECT_PROG __declspec(dllexport)
+#else
+#define PROGECT_PROG __declspec(dllimport)
+#endif
+
+namespace Dlldynamika
+{
+	class PROGECT_PROG Leaver {
+	public:
+		 void leave(std::string name);
+	};
+}
+
+#endif
